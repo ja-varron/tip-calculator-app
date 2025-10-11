1ff90bdcc7e561ebe3777132acb931b039d70e7b
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2025-10-11 16:33:20 +0800

    Updates page background and layout
    Improves the initial visual appearance and layout by setting a specific background color and implementing a flexbox-based structure for centering content on the page. This enhances the user experience by providing a cleaner and more visually appealing landing point.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-blue-300">
+      <body className="bg-[#c5e4e7] justify-center items-start md:items-center flex min-h-screen py-10 md:py-0">
         {children}
       </body>
     </html>
